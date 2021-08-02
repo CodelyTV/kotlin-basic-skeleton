db5298329befcec7b3a8856851e580c04b33a438
Isabel Garrido <isabel.garrido@code.seat>
2021-08-02 20:25:26 +0200

    Configure project

---
+++ build.gradle.kts
@@ -0,0 +1,51 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.21"
+    id("com.diffplug.spotless") version "5.7.0"
+}
+
+group = "com.codely"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("com.diffplug.spotless:spotless-plugin-gradle:5.7.0")
+
+    testImplementation(kotlin("test"))
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+spotless {
+    kotlin {
+        ktlint()
+            .userData(
+                mapOf(
+                    "insert_final_newline" to "true"
+                )
+            )
+    }
+    kotlinGradle {
+        ktlint()
+    }
+}
+
+tasks.check {
+    dependsOn(tasks.spotlessCheck)
+}
