3525f9d464b353af32db55f3a665556c291c6e4a
Isabel Garrido <isabel.garrido@code.seat>
2023-11-24 19:46:57 +0100

    Upgrade Kotlin to 1.9.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.9.0"
+    kotlin("jvm") version "1.9.10"
     id("com.diffplug.spotless") version "6.22.0"
     application
 }
